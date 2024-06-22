b0a881c6caeac07b721a29d92c8b2f6fa285af0c
Nicotertu <nicotertu@gmail.com>
2024-06-22 08:03:41 -0500

    Fixed DAG failing

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -3,10 +3,17 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 import requests
 import datetime
+import os
 
 def batch_predict_task():
+    print("Current working directory:", os.getcwd())
+    print("Files in current directory:", os.listdir('.'))
+    file_path = './opt/airflow/dags/data_playlist.csv'
+    if not os.path.exists(file_path):
+        print(f"File {file_path} does not exist!")
+        return
     url = 'http://localhost:80/batch_predict'
-    files = {'file': open('path_to_your_csv_file.csv', 'rb')}
+    files = {'file': open(file_path, 'rb')}
     response = requests.post(url, files=files)
     print(response.json())
 
